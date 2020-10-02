1721969c4fa4727b8c9df75a48f27d01a60a7908
Shi Han NG <shihanng@gmail.com>
2020-10-02 22:18:53 +0900

    Refactor to newIMAPClient

--- server/mailermost/mailermost.go
+++ server/mailermost/mailermost.go
@@ -75,26 +75,16 @@ func (r *replyToBatchError) Error() string {
 }
 
 func (p *Poller) checkMailbox() error {
-	var (
-		c    *client.Client
-		cerr error
-	)
-
-	if p.security == "none" {
-		c, cerr = client.Dial(p.server)
-	} else {
-		c, cerr = client.DialTLS(p.server, nil)
+	c, err := newIMAPClient(p.server, p.security)
+	if err != nil {
+		return errors.Wrap(err, "failure connecting to IMAP server")
 	}
 
-	if cerr != nil {
-		return errors.Wrap(cerr, "failure connecting to IMAP server")
-	}
-
-	if err := c.Login(p.email, p.password); err != nil {
+	if err = c.Login(p.email, p.password); err != nil {
 		return errors.Wrapf(err, "failure loging into email for user %q", p.email)
 	}
 	defer func() {
-		err := c.Logout()
+		err = c.Logout()
 		if err != nil {
 			p.api.LogError("Failed to log out of mailbox", "error", err.Error())
 		}
@@ -127,6 +117,13 @@ func (p *Poller) checkMailbox() error {
 	}
 
 	return nil
+}
+
+func newIMAPClient(addr, security string) (*client.Client, error) {
+	if security == "none" {
+		return client.Dial(addr)
+	}
+	return client.DialTLS(addr, nil)
 }
 
 func (p *Poller) processEmail(msg *imap.Message, section *imap.BodySectionName, seqset *imap.SeqSet, c *client.Client) {
